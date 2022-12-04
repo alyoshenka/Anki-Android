31d377a2b2d3c67c6bb37db7ecb4d064524fabf3
Alexi <alexibmost@gmail.com>
2022-12-04 12:07:32 -0800

    removing more unnecessary changes

--- AnkiDroid/src/main/java/com/ichi2/anki/reviewer/MappableBinding.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/reviewer/MappableBinding.kt
@@ -14,7 +14,7 @@
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.anki.reviewer // todo: cardviewer?
+package com.ichi2.anki.reviewer
 
 import android.content.Context
 import android.content.SharedPreferences
